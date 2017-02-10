3d2893a8e5c7baccd70b93c546bb354f7cd52105
Abdullah Alsharif <gxr199@gmail.com>
2017-02-10 13:41:27 +0000

    Adding PKColumnA explination

---
+++ R/mutanttimingAliveScript.R
@@ -0,0 +1,25 @@
+dbmss <- as.vector(dplyr::distinct(d, dbms))[[1]]
+cases <- as.vector(dplyr::distinct(d, schema))[[1]]
+generators <- as.vector(dplyr::distinct(d, generator))[[1]]
+
+for (db in dbmss) {
+  for (gen in generators)
+  for (case in cases) {
+    data <- d %>% filter(dbms == db, schema == case, generator != "random")
+    caseStudyFileName <- paste(case, "-" , db,".dat", sep="")
+    fileSaving <- sprintf("mutanttiming/%s",caseStudyFileName)
+    write.csv(data, file = fileSaving)
+
+  }
+}
+
+for (db in dbmss) {
+  for (gen in generators) {
+    if (gen != "random") {
+      data <- d %>% filter(dbms == db, generator == gen)
+      caseStudyFileName <- paste(gen, "-" , db,".dat", sep="")
+      fileSaving <- sprintf("mutanttiming-generators/%s",caseStudyFileName)
+      write.csv(data, file = fileSaving)
+    }
+  }
+}
